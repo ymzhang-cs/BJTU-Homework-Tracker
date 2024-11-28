480e8867613b1ded06f7d1ad16e4635a8ed6abce
Yuanming Zhang <ymzhang.cs@gmail.com>
2024-11-28 23:30:28 +0800

    Query API Code test 3

--- api/homework-query/test/blank_content.py
+++ api/homework-query/test/blank_content.py
@@ -13,7 +13,7 @@ app = flask.Flask(__name__)
 
 @app.route('/api/homework-query', methods=['POST'])
 def query_route():
-    content = [{'title': '补交课后作业1 - 数字系统基础', 'due_time': 'NAT', 'content': '', 'course_name': '数字系统基础'}]
+    content = [{'title': '补交课后作业1 - 数字系统基础', 'due_time': 'NAT', 'content': '没有详情', 'course_name': '数字系统基础'}]
     return flask.jsonify({'result': content})
 
 if __name__ == '__main__':
